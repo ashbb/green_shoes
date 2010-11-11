aa7ae27e0e760330d506990f1e133c6daeaca03a
ashbb <ashbbb@gmail.com>
2010-11-11 23:49:19 +0900

    Changed sample18 a little.

--- samples/sample18.rb
+++ samples/sample18.rb
@@ -2,8 +2,17 @@ require "../lib/green_shoes"
 
 Shoes.app do
   title "Shoes is a ", link("tiny"){alert "Cool!"}, " graphics toolkit. "
-  tagline "It's simple and straightforward. ", 
-    link("Shoes was born to be easy! "){alert "Yay!"}, 
-    "Really, it was made for absolute beginners. "
+
+  flow width: 0.4 do
+    img = image '../static/gshoes-icon.png'
+    img.click{alert "You're soooo quick!"}
+  end
+
+  flow width: 0.6 do
+    tagline "It's simple and straightforward. ", 
+      link("Shoes was born to be easy! "){alert "Yay!"}, 
+      "Really, it was made for absolute beginners. "
+  end
+
   subtitle link("There's really nothing to it. "){alert "Have fun!"}
 end
