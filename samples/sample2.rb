be3ede0658deffee1cee9a2a509ea83e64c7bdc7
ashbb <ashbbb@gmail.com>
2010-12-04 12:24:04 +0900

    Reformed sample2 for the latest version.

--- samples/sample2.rb
+++ samples/sample2.rb
@@ -8,6 +8,6 @@ Shoes.app title: 'Green Shoes New Logo Icon!', width: 300, height: 420 do
       image '../static/gshoes-icon.png'
     end
     image '../static/gshoes-icon.png'
-    para ' ' * 40, 'Created by Zak'
+    para ' ' * 30, 'Created by Zak'
   end
 end
