da7ee55e5433b8e129bcd65716793b4f188e62c7
ashbb <ashbbb@gmail.com>
2010-12-17 22:04:17 +0900

    An imitation of Hackety Hack opening demo. But works for a short time only... sigh. Look this: http://vgoff.posterous.com/an-imitation-of-hackety-hack-opening-demo-wit

---
+++ samples/sample39.rb
@@ -0,0 +1,18 @@
+# An imitation of Hackety Hack opening demo.
+
+require '../lib/green_shoes'
+
+Shoes.app width: 420, height: 420 do
+  nostroke
+  background black
+  title fg(strong('Hackety Hack'), white), margin: 20
+
+  stack do
+    @m = mask{star -284, -174, 130, 500, 90}
+    image "./splash-hand.png" , top: 204, left: 84
+  end
+
+  animate 2 do |i|
+    @m.clear{rotate i; star -284, -174, 130, 500, 90}
+  end
+end
