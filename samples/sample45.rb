b7cadfe941702fbfe8c78e35f8c5b5dbf7be8d5d
ashbb <ashbbb@gmail.com>
2011-01-22 00:18:46 +0900

    Oops, forgot to upload sample45.rb

---
+++ samples/sample45.rb
@@ -0,0 +1,13 @@
+require '../lib/green_shoes'
+
+Shoes.app height: 200 do
+  title 'Hello Green Shoes'
+  font 'Coolvetica'
+  title 'Hello Green Shoes'
+  title 'Hello Green Shoes', font: 'Lucida console'
+  nolayout
+  font nil
+  para 'sans (default)', left: 400, top: 30
+  para 'Coolvetica', left: 400, top: 92
+  para 'Lucida Console', left: 470, top: 155
+end
