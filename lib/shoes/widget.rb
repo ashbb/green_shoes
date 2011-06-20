0e12271562fbcba8df0ad4c3dfc2e6da0a3db4d6
Justin Patera <serialhex@gmail.com>
2011-06-20 00:11:43 -0400

    updated green shoes widget parser to transfor FooWidgetThiny into foo_widget_thingy like red shoes

--- lib/shoes/widget.rb
+++ lib/shoes/widget.rb
@@ -1,9 +1,14 @@
 class Shoes
   class Widget
     def self.inherited klass, &blk
-      m = klass.inspect.downcase.split('::').last
+      puts klass.inspect
+      m = klass.to_s[/(^|::)(\w+)$/, 2].
+              gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+              gsub(/([a-z\d])([A-Z])/,'\1_\2').downcase
+      puts m
       Shoes::App.class_eval do
         define_method m do |*args, &blk|
+          puts klass
           klass.class_variable_set :@@__app__, self
           parent = app.cslot
           klass.new(*args, &blk).tap do |s|
