71a2b5373bc6303212cb1308d5dec8ec4b126e23
ashbb <ashbbb@gmail.com>
2012-10-02 20:36:45 +0900

    Added fullscreen style. Thanks for the request, Ron. Refer to http://librelist.com/browser//shoes/2012/9/27/fullscreen-green-shoes/

--- lib/shoes/main.rb
+++ lib/shoes/main.rb
@@ -20,8 +20,13 @@ class Shoes
     app.top_slot = Flow.new app.slot_attributes(app: app, left: 0, top: 0)
 
     win = get_win
-    win.title = args[:title]
-    win.set_default_size args[:width], args[:height]
+    if args[:fullscreen]
+      win.decorated = false
+      win.maximize
+    else
+      win.title = args[:title]
+      win.set_default_size args[:width], args[:height]
+    end
 
     style = win.style
     style.set_bg Gtk::STATE_NORMAL, 65535, 65535, 65535
